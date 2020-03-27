73d9303e87079bb5f3717874fffe81b230290642
C_Bereket Tolcha <BT11@security-finance.com>
2020-03-26 20:34:39 -0400

    first commit

---
+++ app/src/main/java/com/kidist/bereket/weatherandroidapp/ForecastWeatherActivity.java
@@ -0,0 +1,14 @@
+package com.kidist.bereket.weatherandroidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForecastWeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forecast_weather);
+    }
+}
